5c46a0535c696178443ffb42dcefc177fa91e35f
seokjun01 <suckjungod@naver.com>
2026-08-18 08:38:17 +0900

    shadCn UI 실습

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import "./App.css";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { Input } from "./components/ui/input";
+import { Textarea } from "./components/ui/textarea";
+import { Toaster } from "./components/ui/sonner";
+import { toast } from "sonner";
 
 function App() {
   const isActive = true;
 
   return (
-    <div>
-      <Button>버튼 !</Button>
+    <div className="p-5">
+      <Toaster />
+      <Textarea placeholder="입력 . . . " />
+      <Input placeholder="입력 . . . " />
+
+      <Button
+        onClick={() => {
+          toast("버튼 클릭!");
+        }}
+      >
+        버튼 !
+      </Button>
+      <Button variant={"destructive"}>버튼 !</Button>
+      <Button variant={"ghost"}>버튼 !</Button>
+      <Button variant={"link"}>버튼 !</Button>
+      <Button variant={"outline"}>버튼 !</Button>
+      <Button variant={"secondary"}>버튼 !</Button>
+
       <div className={cn(isActive ? "text-green-500" : "text-red-500")}>
         isActve
       </div>
