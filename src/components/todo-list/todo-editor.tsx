30de524d6e9abeaef8032996a8c940eafc4d3301
seokjun01 <suckjungod@naver.com>
2026-08-18 13:48:00 +0900

    todos 기능 연결

--- src/components/todo-list/todo-editor.tsx
+++ src/components/todo-list/todo-editor.tsx
@@ -1,11 +1,25 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useCreateTodo } from "@/store/todos";
+import { useState } from "react";
 
 export default function TodoEditor() {
+  const createTodo = useCreateTodo();
+  const [content, setContent] = useState("");
+
+  const handleAddClick = () => {
+    if (content.trim() === "") return;
+    createTodo(content);
+    setContent("");
+  };
   return (
     <div className="flex gap-2">
-      <Input placeholder="새로운 할 일을 등록하세요..." />
-      <Button></Button>
+      <Input
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        placeholder="새로운 할 일을 등록하세요..."
+      />
+      <Button onClick={handleAddClick}>추가</Button>
     </div>
   );
 }
