9eac4f5b19c2cf4c2e7414bdac9d78bf7a558107
seokjun01 <suckjungod@naver.com>
2026-08-19 14:12:35 +0900

    Query Key factory

--- src/hooks/quries/use-todo-data-by-id.ts
+++ src/hooks/quries/use-todo-data-by-id.ts
@@ -1,12 +1,13 @@
 import { fetchTodoById } from "@/api/fetch-todo-by-id";
+import { QUERY_KEYS } from "@/lib/constants";
 import { useQuery } from "@tanstack/react-query";
 
-export function useTodoDataById(id: number) {
+export function useTodoDataById(id: string) {
   return useQuery({
     queryFn: () => fetchTodoById(id),
-    queryKey: ["todos", id],
+    queryKey: QUERY_KEYS.todo.detail(id),
 
-    staleTime: 5000,
+    staleTime: 30000,
     gcTime: 5000,
     // refetchOnMount: false,
     // refetchOnWindowFocus: false,
